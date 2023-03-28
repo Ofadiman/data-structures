24d4ebadbf5e826a221d8c0bcccf437d6040c418
ofadiman <ofadiman@gmail.com>
2023-03-28 22:11:22 +0200

    feature(singly_linked_list): implement ForEachNode method

--- singly_linked_list.go
+++ singly_linked_list.go
@@ -1,18 +1,29 @@
 package main
 
+import "fmt"
+
+func NewSinglyLinkedListNode(value int, next *SinglyLinkedListNode) *SinglyLinkedListNode {
+	return &SinglyLinkedListNode{
+		value: value,
+		next:  next,
+	}
+}
+
 type SinglyLinkedListNode struct {
 	value int
 	next  *SinglyLinkedListNode
 }
 
-type SinglyLinkedList struct {
-	head   *SinglyLinkedListNode
-	tail   *SinglyLinkedListNode
-	length int
+func (r *SinglyLinkedListNode) Value() int {
+	return r.value
 }
 
-func NewSinglyLinkedList(values ...int) SinglyLinkedList {
-	list := SinglyLinkedList{
+func (r *SinglyLinkedListNode) Next() *SinglyLinkedListNode {
+	return r.next
+}
+
+func NewSinglyLinkedList(values ...int) *SinglyLinkedList {
+	list := &SinglyLinkedList{
 		head:   nil,
 		tail:   nil,
 		length: 0,
@@ -20,29 +31,27 @@ func NewSinglyLinkedList(values ...int) SinglyLinkedList {
 
 	if len(values) > 0 {
 		for i := 0; i < len(values); i++ {
-			if i == 0 {
-				node := SinglyLinkedListNode{
-					value: values[0],
-					next:  nil,
-				}
+			node := NewSinglyLinkedListNode(values[i], nil)
 
-				list.head = &node
-				list.tail = &node
+			if list.Head() == nil {
+				list.head = node
+				list.tail = node
+			} else {
+				list.tail.next = node
+				list.tail = node
 			}
-
-			node := SinglyLinkedListNode{
-				value: values[i],
-				next:  nil,
-			}
-
-			list.tail.next = &node
-			list.tail = &node
 		}
+
+		list.length = len(values)
 	}
 
-	list.length = len(values)
-
 	return list
+}
+
+type SinglyLinkedList struct {
+	head   *SinglyLinkedListNode
+	tail   *SinglyLinkedListNode
+	length int
 }
 
 func (r *SinglyLinkedList) Size() int {
@@ -55,4 +64,21 @@ func (r *SinglyLinkedList) Head() *SinglyLinkedListNode {
 
 func (r *SinglyLinkedList) Tail() *SinglyLinkedListNode {
 	return r.tail
+}
+
+func (r *SinglyLinkedList) ForEachNode(callback func(node *SinglyLinkedListNode)) {
+	if r.Head() == nil {
+		return
+	}
+
+	node := r.Head()
+	for {
+		if node == nil {
+			break
+		}
+
+		fmt.Printf("calling callback with node: {value: %v}\n", node.value)
+		callback(node)
+		node = node.next
+	}
 }
--- singly_linked_list_test.go
+++ singly_linked_list_test.go
@@ -5,6 +5,25 @@ import (
 	"testing"
 )
 
+func TestNewSinglyLinkedList(t *testing.T) {
+	t.Run("should initialize empty list", func(t *testing.T) {
+		list := NewSinglyLinkedList()
+
+		assert.Nil(t, list.Head())
+		assert.Nil(t, list.Tail())
+	})
+
+	t.Run("should initialize singly linked list with values", func(t *testing.T) {
+		list := NewSinglyLinkedList(1, 2, 3)
+
+		head := list.Head()
+		next := head.Next()
+		assert.Equal(t, head.Value(), 1)
+		assert.Equal(t, next.Value(), 2)
+		assert.Equal(t, list.Head().Next().Next().Value(), 3)
+	})
+}
+
 func TestSinglyLinkedList_Size(t *testing.T) {
 	t.Run("should return 0 if list is empty", func(t *testing.T) {
 		list := NewSinglyLinkedList()
@@ -45,5 +64,33 @@ func TestSinglyLinkedList_Tail(t *testing.T) {
 		list := NewSinglyLinkedList(1, 2, 3)
 
 		assert.Equal(t, list.Tail().value, 3)
+	})
+}
+
+func TestSinglyLinkedList_ForEachNode(t *testing.T) {
+	t.Run("should do nothing if list is empty", func(t *testing.T) {
+		var elements []int
+		callback := func(node *SinglyLinkedListNode) {
+			elements = append(elements, node.value)
+		}
+
+		list := NewSinglyLinkedList()
+
+		list.ForEachNode(callback)
+
+		assert.Len(t, elements, 0)
+	})
+
+	t.Run("should call callback function for each list node", func(t *testing.T) {
+		var elements []int
+		callback := func(node *SinglyLinkedListNode) {
+			elements = append(elements, node.value)
+		}
+
+		list := NewSinglyLinkedList(1, 2, 3)
+
+		list.ForEachNode(callback)
+
+		assert.Equal(t, elements, []int{1, 2, 3})
 	})
 }
